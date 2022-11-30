5bab4d0b90068c8a41e0be4a6c627e26ce1e5bb9
Allan <allanpaiva17@gmail.com>
2022-11-30 11:41:48 -0300

    feat: first func commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var user string = "Alan"
+	var agencyNumber int = 589
+	var accountUser int = 123456
+	var balanceAccount float64 = 125.5
+
+	fmt.Println(user, agencyNumber, accountUser, balanceAccount)
+
+	var user1 string = "Fulano"
+	var agencyNumber1 int = 555
+	var accountUser1 int = 111333
+	var balanceAccount1 float64 = 200
+
+	fmt.Println(user1, agencyNumber1, accountUser1, balanceAccount1)
+
+}
