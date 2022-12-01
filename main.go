31cdcc829f6886dc0b1e879af654035a48f8ef42
Allan <allanpaiva17@gmail.com>
2022-12-01 12:46:15 -0300

    feat: transfer func added

--- main.go
+++ main.go
@@ -9,34 +9,44 @@ type checkingAccount struct {
 	balanceAccount float64
 }
 
-func (c *checkingAccount) toWithdraw(withdrawAccount float64) string {
+func (c *checkingAccount) toWithdraw(withdrawAccount float64) (string, float64) {
 	getCash := withdrawAccount > 0 && withdrawAccount <= c.balanceAccount
 	if getCash {
 		c.balanceAccount -= withdrawAccount
-		return "saque realizado com sucesso"
+		return "saque realizado com sucesso:", c.balanceAccount
 	} else {
-		return "Saldo insificiente"
+		return "Saldo insificiente:", c.balanceAccount
 	}
 }
 
-func (c *checkingAccount) toDeposite(upCash float64) string {
-	depositCash := upCash >= 1 && upCash < c.balanceAccount
+func (c *checkingAccount) toDeposite(upCash float64) (string, float64) {
+	depositCash := upCash > 0
 	if depositCash {
 		c.balanceAccount += upCash
-		return "deposito realizado"
+		return "deposito realizado:", c.balanceAccount
 	} else {
-		return "deposito não efetuado"
+		return "deposito não efetuado:", c.balanceAccount
+	}
+}
+
+func (c *checkingAccount) transf(transfValue float64, accountDestiny *checkingAccount) bool {
+	if transfValue <= c.balanceAccount && transfValue > 0 {
+		c.balanceAccount -= transfValue
+		accountDestiny.toDeposite(transfValue)
+		return true
+	} else {
+		return false
 	}
 }
 
 func main() {
-	silviaAccount := checkingAccount{}
-	silviaAccount.user = "Silvia"
-	silviaAccount.balanceAccount = 500
+	silviaAccount := checkingAccount{user: "Silvia", balanceAccount: 500}
+	gustavoAccount := checkingAccount{user: "Gustavo", balanceAccount: 100}
 
-	fmt.Println(silviaAccount.balanceAccount)
+	status := silviaAccount.transf(500, &gustavoAccount)
 
-	fmt.Println(silviaAccount.toDeposite(0))
+	fmt.Println(status)
 
-	fmt.Println("Saldo atual:", silviaAccount.balanceAccount)
+	fmt.Println(silviaAccount, gustavoAccount)
+
 }
